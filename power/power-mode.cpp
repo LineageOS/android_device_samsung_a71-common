11986f3d14bbe369d78cb06e11a00ec9458a1e2c
Haky86 <hakamelassouad@gmail.com>
2026-06-25 20:00:03 +0200

    a71-common: Kang TARGET_POWERHAL_MODE_EXT from gta4l
    * Picked from https://github.com/LineageOS/android_device_samsung_gta4l-common/commit/1769dfc5f5dff3f650c0af3f11eff54ef3ae0a94.
    
    Change-Id: Ic7d5e8d95a86b6dbc26ff7b7a99de6711b2e88cf

---
+++ power/power-mode.cpp
@@ -0,0 +1,43 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <android-base/file.h>
+
+#include <aidl/android/hardware/power/BnPower.h>
+
+using ::aidl::android::hardware::power::Mode;
+
+namespace aidl {
+namespace android {
+namespace hardware {
+namespace power {
+namespace impl {
+
+bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
+    switch (type) {
+        case Mode::DOUBLE_TAP_TO_WAKE:
+            *_aidl_return = true;
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool setDeviceSpecificMode(Mode type, bool enabled) {
+    switch (type) {
+        case Mode::DOUBLE_TAP_TO_WAKE:
+            ::android::base::WriteStringToFile(enabled ? "aot_enable,1" : "aot_enable,0",
+                                               "/sys/class/sec/tsp/cmd");
+            return true;
+        default:
+            return false;
+    }
+}
+
+}  // namespace impl
+}  // namespace power
+}  // namespace hardware
+}  // namespace android
+}  // namespace aidl
