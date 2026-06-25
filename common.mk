8d2cee307299c66d294205353fd4bdd69f804522
Haky86 <hakamelassouad@gmail.com>
2026-06-25 19:05:01 +0200

    a71-common: Get rid of Pixel libperfmgr HAL
    * The hardware/google/pixel is dead with the latest branch: android-16.0.0_r4, gonna replace with QTI PowerHAL.
    
    Change-Id: I6127ee9aa4c7ae7410c54bc983aa97c2014dbf5d

--- common.mk
+++ common.mk
@@ -290,13 +290,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service.pixel-libperfmgr \
-
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
-
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
